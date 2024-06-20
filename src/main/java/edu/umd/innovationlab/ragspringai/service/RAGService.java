d6d4ddd3ed8b6cf423d96217202936e41c4e4b4e
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-06-20 10:31:04 -0400

    Set up dependencies and import statements.

--- src/main/java/edu/umd/innovationlab/ragspringai/service/RAGService.java
+++ src/main/java/edu/umd/innovationlab/ragspringai/service/RAGService.java
@@ -2,24 +2,39 @@ package edu.umd.innovationlab.ragspringai.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Service;
+
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.document.Document;
+import java.util.List;
+
 
 @Service
 public class RAGService {
 
+    private final EmbeddingModel embeddingModel;
+
+
     final private Logger logger = LoggerFactory.getLogger(RAGService.class);
 
-    public RAGService() {
+    public String getEmbedding(String text) {
+        return embeddingModel.embed(text).toString();
+    }
+
+    public RAGService(EmbeddingModel embeddingModel) {
+        this.embeddingModel = embeddingModel;
     }
 
     public String doRAG(String input) {
         throw new UnsupportedOperationException("Not Implemented");
     }
 
+    /*
     public String getEmbedding(String text) {
         throw new UnsupportedOperationException("Not Implemented");
     }
-
+*/
     public String chat(String text) {
         throw new UnsupportedOperationException("Not Implemented");
     }
