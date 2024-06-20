f6876751415b437cb07b3258b8c3c5fbda9fabe6
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-06-20 11:02:35 -0400

    Edited RAG Service

--- src/main/java/edu/umd/innovationlab/ragspringai/service/RAGService.java
+++ src/main/java/edu/umd/innovationlab/ragspringai/service/RAGService.java
@@ -2,18 +2,22 @@ package edu.umd.innovationlab.ragspringai.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.DocumentReader;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.stereotype.Service;
-
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.document.Document;
 import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 
 
 @Service
 public class RAGService {
 
     private final EmbeddingModel embeddingModel;
+
+    private final VectorStore vectorStore;
 
 
     final private Logger logger = LoggerFactory.getLogger(RAGService.class);
@@ -22,8 +26,17 @@ public class RAGService {
         return embeddingModel.embed(text).toString();
     }
 
-    public RAGService(EmbeddingModel embeddingModel) {
+    public RAGService(EmbeddingModel embeddingModel, VectorStore vectorStore) {
         this.embeddingModel = embeddingModel;
+        this.vectorStore = vectorStore;
+    }
+
+    public String loadData(String fileName) {
+        DocumentReader pdfDocumentReader = new PagePdfDocumentReader(new ClassPathResource("data/"+fileName));
+        List<Document> documents = pdfDocumentReader.get();
+        vectorStore.add(documents);
+        return "loaded " + fileName;
+
     }
 
     public String doRAG(String input) {
@@ -44,10 +57,6 @@ public class RAGService {
     }
 
     public String search(String text) {
-        throw new UnsupportedOperationException("Not Implemented");
-    }
-
-    public String loadData(String fileName) {
         throw new UnsupportedOperationException("Not Implemented");
     }
 
